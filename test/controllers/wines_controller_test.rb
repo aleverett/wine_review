c5ad3c8f1417850aca95715001c4cbf477d2bb43
Al Everett <al.everett@evanschambers.com>
2015-04-04 14:54:39 -0400

    Added some tests

--- test/controllers/wines_controller_test.rb
+++ test/controllers/wines_controller_test.rb
@@ -4,11 +4,15 @@ class WinesControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_not_nil assigns(:wines)
   end
 
   test "should get show" do
-    get :show
+    get :show, id: wines(:testwine).id
     assert_response :success
+    wine = assigns(:wine)
+    assert_not_nil wine
+    assert_equal 'Good Wine', wine.name
   end
 
 end
--- test/models/wine_test.rb
+++ test/models/wine_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class WineTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "persistence works" do
+    created_wine = Wine.create(name:'Created', year:1999,winery:'Winery',country:'Mexico',varietal:'Merlot')
+    found_wine = Wine.find created_wine.id
+    assert_not_nil found_wine
+    assert_equal 'Created', found_wine.name
+    assert_equal 1999, found_wine.year
+    assert_equal 'Winery', found_wine.winery
+    assert_equal 'Mexico', found_wine.country
+    assert_equal 'Merlot', found_wine.varietal
+  end
+
+  test "update from a fixture" do
+    wine = wines(:testwine)
+    assert_equal 2000, wine.year
+    wine.year = 2001
+    wine.save
+    updated = Wine.find(wine.id)
+    assert_equal 2001, wine.year
+  end
 end
