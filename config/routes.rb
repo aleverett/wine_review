7a9f288263727cecd210b0b4be095d25325b4724
Al Everett <al.everett@evanschambers.com>
2015-04-04 14:25:10 -0400

    fixed routes.rb; cleaned up index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'wines/index'
+  get "/wines" => "wines#index", as: 'wines'
 
-  get 'wines/show'
+  get "/wines/:id" => "wines#show", as: 'wine'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
