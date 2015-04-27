6a058d9e95ff30ad2ee7e9c7c3742da5a27fd6fc
Al Everett <al.everett@evanschambers.com>
2015-04-27 19:42:01 -0400

    added initial error trapping

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,2 +1,7 @@
 class Wine < ActiveRecord::Base
+  VARIETALS = ['Chardonnay', 'Sauvignon blanc', 'Semillon', 'Moscato', 'Pinot grigio', 'Gewurztraminer', 'Riesling', 'Syrah', 'Merlot', 'Cabernet Sauvignon', 'Malbec', 'Pinot noir', 'Zinfandel', 'Sangiovese', 'Barbera']
+
+  validates :name, :year, :country, presence: true
+  validates :year, numericality: { only_integer: true }, unless: "year.blank?"
+  validates :varietal, inclusion: { in: VARIETALS }
 end
